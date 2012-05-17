e7b442e5bc1fb5e5b812856358b138221ea365ab
dryman <idryman@gmail.com>
2012-05-17 11:03:48 +0800

    can work on Chinese nowgit st

--- macho.js
+++ macho.js
@@ -1,5 +1,21 @@
 (function( $ ) {
-  $.fn.macho = function () {
+  $.fn.macho = function (len) {
+    len = len || 3;
+    var puncs_reg = new RegExp("[-,_\\|<.>/?;:'\"`~!@#$%^&*()（）´・ωつдС；∀ﾟo彡★☆▽￣╮╭ノ╰〒皿～┴‵□′↗︴yΦθ↖，。？！：；＠m＃＄％︿＆＊＝＋艸凸∩＿ˍ▁▂▃▄▅▆█◣◎]+$");
 
+    this.each(function(idx){
+      var punc = $(this).html().match(puncs_reg); // faild if end with </em> or </strong>
+      if (punc!=null && punc.length > 3) 
+        return $(this).html($(this).html().replace(puncs_reg,"<span class='macho'>$&</span>")); // works like continue
+
+      for (l=0;l<len;l++){
+        var reg = new RegExp("\\w+\\W{"+l+"}"+punc+"$");
+        var res = $(this).html().match(reg);
+        // note: does not handle the case that the english string is too short
+        if (res!=null) return $(this).html($(this).html().replace(reg,"<span class='macho'>$&</span>"));
+      }
+      var reg = new RegExp("\\W{"+len+"}"+punc+"$");
+      $(this).html($(this).html().replace(reg,"<span class='macho' style='display: inline-block;'>$&</span>"));
+    });
   };
 })( jQuery );

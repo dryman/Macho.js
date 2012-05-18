b54f8570ffa97c63ce237d6d7a7c8cec9d6a6689
dryman <idryman@gmail.com>
2012-05-18 10:15:24 +0800

    added html escaped caracter handling

--- macho.js
+++ macho.js
@@ -3,18 +3,25 @@
     len = len || 3;
     var puncs_reg = new RegExp("[-,_\\|<.>/?;:'\"`~!@#$%&*()（）‧´・ωつдС；∀ﾟo彡★☆▽￣╮╭ノ╰〒皿～┴‵□′↗︴yΦθ↖，。？！：；＠m＃＄％︿＆＊＝＋╰╯崩潰艸凸∩＿ˍ▁▂▃▄▅▆▇◣◎█◢^]+$");
 
+    // TODO: need to reset if there is already <span class='macho'> tags in html
+    // TODO: handle special characters like &amp
     var genReg = function (acc, len, html){
       if (len === 0) {
         return new RegExp(acc);
       } 
       else {
-        var reg = new RegExp("(\\w+)"+acc);
+        /* res[1] = ((\w+)|(&\w{1,5};))
+         * res[2] = (\w+)
+         * res[3] = (&\w{1,5};)
+         */
+        var reg = new RegExp("((\\w+)|(&\\w{1,5};))"+acc); 
         var res = reg.exec(html);
         if (res != null) {
-          if (res[1].length > 3) { // Length of (\w+) > 3
+          if (res[2] != null && res[2].length > 3) { // Length of (\w+) > 3
             return reg;
           } else {
-            // Treat (\w+) as 1 wide unicode charater, use it as accumulator
+            // Short word or html escaped caracter are treated as one wide
+            // caracter (like one Chinese caracter)
             return genReg(res[0]+"$", --len, html); 
           }
         } else {
@@ -26,7 +33,6 @@
     var output = "<span class='macho' style='display: inline-block;'>$&</span>";
       
 
-    // TODO: need to reset if there is already <span class='macho'> tags in html
     this.each(function(idx){
       var punc = $(this).html().match(puncs_reg); // faild if end with </em> or </strong>
       if (punc!=null && punc[0].length > 3) 

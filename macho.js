c27b5914f529cbb366136a0a0e1e7d01fa5a4a44
dryman <idryman@gmail.com>
2012-05-19 14:55:43 +0800

    switch to beta stage

--- macho.js
+++ macho.js
@@ -9,23 +9,19 @@
       "<span class='macho' style='display: inline-block;'>$&</span>" :
       "<span class='macho'>$&</span>";
 
-    // TODO: need to reset if there is already <span class='macho'> tags in html
-    // TODO: If there are some regexp strings in acc, there will be a problem
-    // TODO: Should use XRegExp module and there is regexp escape function that
-    // can solve the problem
     var reg_escape = function (str) {
       return str.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
     }
     var genReg = function (acc, len, html){
       if (len === 0) {
-        return new XRegExp(acc);
+        return new RegExp(acc);
       } 
       else {
         /* res[1] = ((\w+)|(&\w{1,5};))
          * res[2] = (\w+)
          * res[3] = (&\w{1,5};)  // will fail if there is '#' in \w
          */
-        var reg = new XRegExp("((\\w+)|(&\\w{1,5};))"+acc); 
+        var reg = new RegExp("((\\w+)|(&\\w{1,5};))"+acc); 
         var res = reg.exec(html);
         if (res != null) {
           if (res[2] != null && res[2].length > 3) { // Length of (\w+) > 3
@@ -33,7 +29,7 @@
           } else {
             // Short word or html escaped character are treated as one wide
             // character (like one Chinese character)
-            return genReg(res[0]+"$", --len, html); 
+            return genReg(reg_escape(res[0])+"$", --len, html); 
           }
         } else {
             // Add one non ascii charater to accumulator
@@ -43,15 +39,13 @@
     };
       
 
-    var puncs_reg = new XRegExp("[-,_\\|<.>/?;:'\"`~!@#$%&*()（）‧´・ωつдС；∀ﾟo彡★☆▽￣╮╭ノ╰〒皿～┴‵□′↗︴yΦθ↖，。？！：；＠m＃＄％︿＆＊＝＋╰╯崩潰艸凸∩＿ˍ▁▂▃▄▅▆▇◣◎█◢^]+$");
+    var puncs_reg = new RegExp("[-,_\\|<.>/?;:'\"`~!@#$%&*()（）‧´・ωつдС；∀ﾟo彡★☆▽￣╮╭ノ╰〒皿～┴‵□′↗︴yΦθ↖，。？！：；＠m＃＄％︿＆＊＝＋╰╯崩潰艸凸∩＿ˍ▁▂▃▄▅▆▇◣◎█◢^]+$");
     this.each(function(idx){
       if ($(this).html().match(/</)) return true; // do nothing if other tags are found.
 
       var punc = $(this).html().match(puncs_reg); 
       if (punc!=null && punc[0].length > 3) 
         return $(this).html($(this).html().replace(puncs_reg,output)) || true; // works like continue
-
-      console.log(idx,punc);
 
       var reg = genReg(reg_escape(punc[0])+"$", len, $(this).html());
       return $(this).html($(this).html().replace(reg,output)) || true;
